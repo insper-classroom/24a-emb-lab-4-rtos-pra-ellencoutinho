5ec5a2e4e306e335a41e4fb18ee7bb160f8662cf
ellencoutinho <ellencls@al.insper.edu.br>
2024-03-20 00:35:43 +0000

    Imprime distância no terminal

--- main/main.c
+++ main/main.c
@@ -20,6 +20,11 @@ const uint LED_1_OLED = 20;
 const uint LED_2_OLED = 21;
 const uint LED_3_OLED = 22;
 
+int ECHO_PIN = 13; 
+int TRIG_PIN = 16;
+
+SemaphoreHandle_t xSemaphore_trig;
+
 void oled1_btn_led_init(void) {
     gpio_init(LED_1_OLED);
     gpio_set_dir(LED_1_OLED, GPIO_OUT);
@@ -124,10 +129,58 @@ void oled1_demo_2(void *p) {
     }
 }
 
+
+void trigger_task(void *p) {
+    gpio_init(TRIG_PIN);
+    gpio_set_dir(TRIG_PIN, GPIO_OUT);
+    gpio_put(TRIG_PIN, 0);
+
+    int delay = 250;
+
+    while (true) {
+        if (xSemaphoreTake(xSemaphore_trig, pdMS_TO_TICKS(500)) == pdTRUE) {
+            gpio_put(TRIG_PIN, 1);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+            gpio_put(TRIG_PIN, 0);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+        }   
+    }
+}
+
+void echo_task(void *p) {
+    uint32_t start_us = 0;
+    gpio_init(ECHO_PIN);
+    gpio_set_dir(ECHO_PIN, GPIO_IN);
+    gpio_pull_up(ECHO_PIN);
+
+    while (true) {
+        if (!gpio_get(ECHO_PIN)) { // se echo==0
+            while (!gpio_get(ECHO_PIN)) { // espera echo ser 1
+                vTaskDelay(pdMS_TO_TICKS(1));
+            }
+            start_us = to_us_since_boot(get_absolute_time());
+        }   
+        if (gpio_get(ECHO_PIN)) { // se echo==1
+            while (gpio_get(ECHO_PIN)) { // espera echo ser 0
+                vTaskDelay(pdMS_TO_TICKS(1));
+            }
+            uint32_t delta_t = to_us_since_boot(get_absolute_time()) - start_us;
+            double distancia =  (340* delta_t/10000)/2.0;
+            printf("Distancia %lf cm \n", distancia);
+            xSemaphoreGive(xSemaphore_trig);
+        }
+    }
+ 
+}
+
 int main() {
     stdio_init_all();
+    
+    xSemaphore_trig = xSemaphoreCreateBinary();
 
-    xTaskCreate(oled1_demo_2, "Demo 2", 4095, NULL, 1, NULL);
+    xTaskCreate(trigger_task, "Trigger_task", 256, NULL, 1, NULL);
+    xTaskCreate(echo_task, "Echo_task", 256, NULL, 1, NULL);
+    xSemaphoreGive(xSemaphore_trig);
 
     vTaskStartScheduler();
 
